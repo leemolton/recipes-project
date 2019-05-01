71dbbaba2fda4b8c0b9fba63204a6ad5749c7643
Lee Molton <leemolton@hotmail.co.uk>
2019-05-01 17:19:50 +0000

    Updated routes in run.py and home page

--- run.py
+++ run.py
@@ -21,8 +21,8 @@ def index():
 
 @app.route('/get_recipes')
 def get_recipes():
-    print(mongo.db.myrecipe.find())
-    return render_template("recipes.html", recipes=mongo.db.myrecipe.find())
+    recipe = mongo.db.recipe.find({"_id": ObjectId()})
+    return render_template("recipes.html")
     
     
 @app.route('/add_recipe')
@@ -30,36 +30,18 @@ def add_recipe():
     return render_template("add.html", categories=mongo.db.categories.find())
     
 
-@app.route('/insert_recipe')
-def insert_recipe():
-    recipes = mongo.db.myrecipe
-    recipes.insert_one(request.form.to_dict())
-    return redirect(url_for('get_recipes'))
-    
-    
-@app.route('/edit_recipes/<recipe_id>')
-def edit_recipes(recipe_id):
-    the_recipes = mongo.db.myrecipe.find_one({"_id": ObjectId(recipe_id)})
-    all_categories = mongo.db.categories.find()
-    return render_template('edit.html', recipes=the_recipes, categories=all_categories)
+#@app.route('/insert_recipe')
+#def insert_recipe():
+#    recipes = mongo.db.recipe
+#    recipes.insert_one(request.form.to_dict())
+#    return redirect(url_for('get_recipes'))
     
 
-@app.route('/recipes/<myrecipe_id>')
-def view_recipe(myrecipe_id):
-    the_recipe = mongo.db.myrecipe.find_one({"_id": ObjectId(myrecipe_id)})
-    return render_template('recipes.html', recipe=the_recipe)
+@app.route('/view_recipe/<recipe_id>')
+def view_recipe(recipe_id):
+    the_recipe = mongo.db.recipe.find_one({"_id": ObjectId(recipe_id)})
+    return render_template('viewrecipe.html', recipe=the_recipe)
     
-    
-#@app.route('/add', methods=["GET", "POST"])
-#def add():
-#    cursor=connection.cursor()
-#    if request.method == "POST":
-#        Name=request.form['name']
-#        Country=request.form['country']
-#        Cuisine=request.form['recipe']
-#        Item=request.form['item']
-#        Quantity=request.form['quantity']
-#        Allergen=request.form['allergen']
         
         # Create a new record
 #    Query = "INSERT INTO Authors (Name, Recipe, CountryID) VALUES(%s,%s,%s)"
