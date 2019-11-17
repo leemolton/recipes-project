d8565d0418d181253fa8e09b4fb88d57d006b26f
Lee Molton <leemolton@hotmail.co.uk>
2019-11-17 14:40:40 +0000

    Added run.py after pulling previous file

---
+++ run.py
@@ -0,0 +1,116 @@
+import os
+import json
+<<<<<<< HEAD
+import pymysql
+from flask import Flask, render_template, redirect, request, url_for, flash
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.secret_key = 'some_secret'
+app.config['MONGO_DBNAME'] = 'myrecipedb'
+app.config['MONGO_URI'] = 'mongodb://root:Everton9@ds145881.mlab.com:45881/myrecipedb'
+
+mongo = PyMongo(app)
+
+# Get username from Cloud9 workspace
+# (modify this variable if running on another environment)
+username = os.getenv('C9_USER')
+=======
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = 'some_secret'
+
+>>>>>>> 016e4a7c1423d84ce28c9141a97d9d786004f1df
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+<<<<<<< HEAD
+
+    
+
+@app.route('/get_recipes')
+def get_recipes():
+        return render_template("recipes.html", recipes=mongo.db.recipe.find())
+
+
+    
+    
+@app.route('/add_recipe')
+def add_recipe():
+        categories=mongo.db.categories.find()
+        return render_template("add.html")
+
+
+    
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+        recipes = mongo.db.recipe
+        recipes.insert_one(request.form.to_dict())
+        flash('Thank you for your recipe!')
+        return redirect(url_for('add_recipe'))
+
+
+    
+
+@app.route('/view_recipe/<recipe_id>')
+def view_recipe(recipe_id):
+        the_recipe = mongo.db.recipe.find_one({'_id': ObjectId(recipe_id)})
+        return render_template('viewrecipe.html', recipe=the_recipe)
+
+
+    
+    
+@app.route('/find_recipe', methods=['GET', 'POST'])
+def find_recipe():
+        if request.method=='POST':
+        
+            # get search term
+            search_term = request.form.get("search_term")
+        
+            # create the index
+            mongo.db.recipe.create_index( [("$**", 'text')] )
+        
+            # search with the search term that came through the search bar
+            cursor = mongo.db.recipe.find({ "$text": { "$search": search_term } })
+            recipes = [recipe for recipe in cursor]
+            print(search_term)
+        
+            # send recipes to page
+            return render_template('search.html', recipes=recipes, query=search_term)
+        
+        return render_template('search.html')
+        
+    
+    
+        
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
+            
+=======
+    
+@app.route('/add', methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+       flash("Thanks {}, we have received your recipe!".format(
+            request.form["name"]))
+    return render_template("add.html", page_title="Add recipe")
+
+    
+@app.route('/view')
+def view():
+    return render_template("view.html", page_title="View recipes")
+
+    
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
+>>>>>>> 016e4a7c1423d84ce28c9141a97d9d786004f1df
